0d2fbbcf810584feac2f1e0fb76d5089e506dca4
Gabriel Sanchez <gsanchez@csumb.edu>
2018-02-15 01:23:16 +0000

    Adding Gabriel Changes

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
        // echo "<img src= './img/cards/clubs/2.png'/>";
         
         $player1 = array('name' => 'Judith', 'imgURL' =>'https://78.media.tumblr.com/513aaf30566f6308d379b9a76a71c458/tumblr_otox3fKZ9Y1s05s0mo1_500.png' , 'hand' => array(), 'points' => 0);
-        $player2 = array('name' => 'Chino',  'hand' => array(), 'points' => 0);
+        $player2 = array('name' => 'Gabriel', 'imgURL' => 'user_img/german.png', 'hand' => array(), 'points' => 0);
         $player3 = array('name' => 'Irais', 'hand' => array(), 'points' => 0);
         $player = array('name' => 'ThePleb',  'hand' => array(), 'points' => 0);
         
