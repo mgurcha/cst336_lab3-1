58c8061c1d8afac7be15ee3985ae4c5d9ff506c9
Judith Ramirez <judramirez@csumb.edu>
2018-02-15 01:12:58 +0000

    initial files

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+    
+    <body>
+        <?php
+       // echo "<img src= './img/cards/clubs/2.png'/>";
+        
+        $player1 = array('name' => 'Judith', 'imgURL' =>'https://78.media.tumblr.com/513aaf30566f6308d379b9a76a71c458/tumblr_otox3fKZ9Y1s05s0mo1_500.png' , 'hand' => array(), 'points' => 0);
+        $player2 = array('name' => 'Chino',  'hand' => array(), 'points' => 0);
+        $player3 = array('name' => 'Irais', 'hand' => array(), 'points' => 0);
+        $player = array('name' => 'ThePleb',  'hand' => array(), 'points' => 0);
+        
+        $allPlayers = array(
+            $player1,
+            $player2,
+            $player3,
+            $player,
+            );
+            
+            function printGame($allPlayers){
+                foreach ($allPlayers as $player) {
+                    //echo "<img src= './img/cards/clubs/2.png'/>";
+                    echo "<img src= '". $player['imgURL'] . "'/>";
+                    echo $player['name'] . "<br/>";
+                }
+            }
+            
+        printGame($allPlayers);
+        ?>
+    </body>
+</html>
