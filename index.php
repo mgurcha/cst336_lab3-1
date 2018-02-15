35e5a3a91b134f776e3745de73d1048ae5aaeed4
Marco Aguilera <maaguilera@csumb.edu>
2018-02-15 01:49:08 +0000

    Added an image of Marco

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         
         $player1 = array('name' => 'Judith', 'imgURL' =>'https://78.media.tumblr.com/513aaf30566f6308d379b9a76a71c458/tumblr_otox3fKZ9Y1s05s0mo1_500.png' , 'hand' => array(), 'points' => 0);
         $player2 = array('name' => 'Gabriel', 'imgURL' => 'user_img/german.png', 'hand' => array(), 'points' => 0);
-        $player3 = array('name' => 'Irais', 'hand' => array(), 'points' => 0);
+        $player3 = array('name' => 'Marco', 'imgURL => user_img/marco.jpg','hand' => array(), 'points' => 0);
         $player = array('name' => 'Manjit', 'imgURL' => 'user_img/blue_bird.jpg', 'hand' => array(), 'points' => 0);
         
         $allPlayers = array(
@@ -24,7 +24,7 @@
                 foreach ($allPlayers as $player) {
                     //echo "<img src= './img/cards/clubs/2.png'/>";
                     echo "<img src= '". $player['imgURL'] . "'/>";
-                    echo $player['name'] . "<br/>";
+                    echo $player['name'] . "<br/> <br />";
                 }
             }
             
