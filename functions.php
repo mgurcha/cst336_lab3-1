5ddfddc10d2131d0a9a7007541f307c0610a0ea3
Marco Aguilera <maaguilera@csumb.edu>
2018-02-20 17:59:51 +0000

    Fixed the duplicate card issue.

--- functions.php
+++ functions.php
@@ -1,40 +1,35 @@
 <?php
-    // global $player1 = array('name' => 'Judith', 'imgURL' =>'https://78.media.tumblr.com/513aaf30566f6308d379b9a76a71c458/tumblr_otox3fKZ9Y1s05s0mo1_500.png' , 'hand' => array(), 'points' => 0);
+    
     $cards = array();
+    $player1 = array('name' => 'Judith', 'imgURL' =>'https://78.media.tumblr.com/513aaf30566f6308d379b9a76a71c458/tumblr_otox3fKZ9Y1s05s0mo1_500.png' , 'hand' => array(), 'points' => 0);
     $player2 = array('name' => 'Gabriel', 'imgURL' => 'user_img/german.png', 'hand' => array(), 'points' => 0);
-    // global $player3 = array('name' => 'Marco', 'imgURL => user_img/marco.jpg','hand' => array(), 'points' => 0);
-    // global $player = array('name' => 'Manjit', 'imgURL' => 'user_img/blue_bird.jpg', 'hand' => array(), 'points' => 0);
-    $field = array($player2);
-
-    function play(){
-        if(count(['player1']['cards']) == 0){
-            for($i = 0; $i < 2; $i++){
-                generateCard(['player1']);
-            }
-            for($i = 0; $i < 2; $i++){
-                generateCard(['player2']);
-            }
-        }
-    }
+    $player3 = array('name' => 'Marco', 'imgURL' => 'user_img/marco.jpg','hand' => array(), 'points' => 0);
+    $player4 = array('name' => 'Manjit', 'imgURL' => 'user_img/blue_bird.jpg', 'hand' => array(), 'points' => 0);
+    $field = array(
+        $player1,
+        $player2,
+        $player3,
+        $player4
+    );
     
     function printGame($allPlayers){
         foreach ($allPlayers as $player) {
-        //echo "<img src= './img/cards/clubs/2.png'/>";
-            echo "<img src='".$player['imgURL']."'/>";
-            echo $player['name'] . "<br/> <br />";
+            echo "<div>";
+            echo "<img src='".$player['imgURL']."' width='200' height='250'/>";
             displayHand($player);
+            echo $player['name'] . "<br/> <br />";
+            echo "</div>";
         }
     }
     
     function displayHand(&$player){
-        echo "<div>";
+        
         for($i = 0; $i < count($player['hand']);$i++){
             echo "<img src=" .$player['hand'][$i] . " />";
         }
-        echo "</div>";
     }
     
-    function generateCard(&$player){
+    function generateCard(&$player, &$cards){
         $temp = 0;
         $cardpath = "";
         
@@ -57,22 +52,7 @@
                 break;
             }   
           
-        }while ($cards[$cardpath] == 1);
-        
-        /* USED TO KEEP SCORE
-        if($temp != 1){
-            $temp = 10;
-        }
-        else{
-            $temp = 11;
-        }
-        
-        if($temp == 11 && $player["total"] + $temp > 21){
-            $player["total"]++;
-        }
-        else {
-            $player["total"] + $temp;
-        }*/
+        }while($cards[$cardpath] != null);
         
         $cards[$cardpath] = 1;
         array_push($player["hand"], $cardpath);
--- index.php
+++ index.php
@@ -10,7 +10,9 @@
     <body>
         <?php
             for($i = 0; $i < 4; $i++){
-                generateCard($field[0]);
+                for($j = 0; $j < 4; $j++){
+                    generateCard($field[$i], $cards);
+                }
             }
             printGame($field);
         ?>
