d563f14a64400268e862bf7683219d33c442ad60
Marco Aguilera <maaguilera@csumb.edu>
2018-02-20 03:12:05 +0000

    Moved functions in index.php and added them to functions.php

--- functions.php
+++ functions.php
@@ -1,17 +1,11 @@
 <?php
-    global $player1 = array('name' => 'Judith', 'imgURL' =>'https://78.media.tumblr.com/513aaf30566f6308d379b9a76a71c458/tumblr_otox3fKZ9Y1s05s0mo1_500.png' , 'hand' => array(), 'points' => 0);
-    // global $player2 = array('name' => 'Gabriel', 'imgURL' => 'user_img/german.png', 'hand' => array(), 'points' => 0);
+    // global $player1 = array('name' => 'Judith', 'imgURL' =>'https://78.media.tumblr.com/513aaf30566f6308d379b9a76a71c458/tumblr_otox3fKZ9Y1s05s0mo1_500.png' , 'hand' => array(), 'points' => 0);
+    $cards = array();
+    $player2 = array('name' => 'Gabriel', 'imgURL' => 'user_img/german.png', 'hand' => array(), 'points' => 0);
     // global $player3 = array('name' => 'Marco', 'imgURL => user_img/marco.jpg','hand' => array(), 'points' => 0);
     // global $player = array('name' => 'Manjit', 'imgURL' => 'user_img/blue_bird.jpg', 'hand' => array(), 'points' => 0);
-    global $field = array{
-        $player1,
-        $player2,
-        $player3,
-        $player4,
-    };
-      
-      
-    
+    $field = array($player2);
+
     function play(){
         if(count(['player1']['cards']) == 0){
             for($i = 0; $i < 2; $i++){
@@ -20,6 +14,15 @@
             for($i = 0; $i < 2; $i++){
                 generateCard(['player2']);
             }
+        }
+    }
+    
+    function printGame($allPlayers){
+        foreach ($allPlayers as $player) {
+        //echo "<img src= './img/cards/clubs/2.png'/>";
+            echo "<img src='".$player['imgURL']."'/>";
+            echo $player['name'] . "<br/> <br />";
+            displayHand($player);
         }
     }
     
@@ -56,6 +59,7 @@
           
         }while ($cards[$cardpath] == 1);
         
+        /* USED TO KEEP SCORE
         if($temp != 1){
             $temp = 10;
         }
@@ -68,10 +72,10 @@
         }
         else {
             $player["total"] + $temp;
-        }
+        }*/
         
-        $_SESSION['cards'][$cardpath] = 1;
-        array_push($player["cards"], $cardpath);
+        $cards[$cardpath] = 1;
+        array_push($player["hand"], $cardpath);
     }
     
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
+<?php
+    include 'functions.php';
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -6,69 +9,10 @@
     
     <body>
         <?php
-       // echo "<img src= './img/cards/clubs/2.png'/>";
-        
-        $player1 = array('name' => 'Judith', 'imgURL' =>'https://78.media.tumblr.com/513aaf30566f6308d379b9a76a71c458/tumblr_otox3fKZ9Y1s05s0mo1_500.png' , 'hand' => array(), 'points' => 0);
-        $player2 = array('name' => 'Gabriel', 'imgURL' => 'user_img/german.png', 'hand' => array(), 'points' => 0);
-        $player3 = array('name' => 'Marco', 'imgURL => user_img/marco.jpg','hand' => array(), 'points' => 0);
-        $player4 = array('name' => 'Manjit', 'imgURL' => 'user_img/blue_bird.jpg', 'hand' => array(), 'points' => 0);
-        
-        $allPlayers = array(
-            $player1,
-            $player2,
-            $player3,
-            $player4,
-            );
-            
-            function printGame($allPlayers){
-                foreach ($allPlayers as $player) {
-                    //echo "<img src= './img/cards/clubs/2.png'/>";
-                    echo "<img src= '". $player['imgURL'] . "'/>";
-                    echo $player['name'] . "<br/> <br />";
-                }
+            for($i = 0; $i < 4; $i++){
+                generateCard($field[0]);
             }
-            
-        printGame($allPlayers);
-        
-        //create array of 52 cards
-        //each card an associative array ==> suit, rank, imgURL, points
-        //popOff one card a time to generate the hand
-
-                
-        function getImgURL($index){
-                    
-                    //get number 0 to 51
-                    //return imgURL
-                    
-                    $suitIndex = floor ($index / 13);
-                    echo "suitIndex: $suitIndex";
-                    
-                    switch ($suitIndex) {
-                        case 'value':
-                            // code...
-                            break;
-                        
-                        default:
-                            // code...
-                            break;
-                    }
-                    
-                    // switch
-                }
-                
-                function generateDeck(){
-                    for($i=0; $i<51; $i++){
-                        $card = array(
-                            'imgURL' => ""
-                            );
-                    }
-                    
-                    
-                }
-                
-                function getHand(){
-                    
-                }
+            printGame($field);
         ?>
     </body>
 </html>
