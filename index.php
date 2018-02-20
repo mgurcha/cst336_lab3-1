3cc5b95be09b3c7608c825dac02fed051f3cb7f7
Judith Ramirez <judramirez@csumb.edu>
2018-02-20 05:11:58 +0000

    All players

--- index.php
+++ index.php
@@ -9,10 +9,13 @@
     
     <body>
         <?php
+            for($i =0; $i< 4;$i++){
+                
                 generateCard($field[0]);
                 generateCard($field[1]);
                 generateCard($field[2]);
                 generateCard($field[3]);
+            }
             
             printGame($field);
         ?>
